0f8cdfa6eed6156b1f28811ab513057032e40fe9
lizunlong <lizunlong@gmail.com>
2023-05-17 15:54:09 +0800

    将缺失值 NaN 替换为空字符串 ''

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ def parse_excel(file):
     # 读取 Excel 文件
     df = pd.read_excel(file)
 
+    # 将缺失值 NaN 替换为空字符串 ''
+    df = df.fillna('')
+
     # 将 DataFrame 转成字典格式
     data = df.to_dict(orient='records')
 
