5e6eca09c80becad143c4ad722f07c9965513054
lizunlong <lizunlong@gmail.com>
2023-05-17 15:31:09 +0800

    允许跨域访问

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 import json
 
 app = Flask(__name__)
+CORS(app) # 允许跨域访问
 
 def parse_excel(file):
     # 读取 Excel 文件
