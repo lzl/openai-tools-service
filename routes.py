ec294de33564cf1a892c6ca52152c6db94c65e31
lizunlong <lizunlong@gmail.com>
2023-05-23 19:20:59 +0800

    clean code

--- routes.py
+++ routes.py
@@ -1,55 +1,38 @@
 from flask import Blueprint, request, jsonify, Response
 import os
-import openai
 import json
-import uuid
-import random
-import time
+import base64
+import openai
 from google.cloud import tasks_v2
+from google.cloud import firestore
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import *
-import base64
 from utils import parse_excel, generate_excel
-from google.cloud import firestore
 
 db = firestore.Client(project='withcontextai')
-
-# global_emails_store = {}
-# global_sheets_store = {}
-# global_questions_store = {}
-# global_answers_store = {}
+api_url = 'https://openai-tools-mmxbwgwwaq-uw.a.run.app'
 
 main_routes = Blueprint('main_routes', __name__)
 
 
 @main_routes.route('/result', methods=['GET'])
 def result_route():
-    request_id = request.args.get('request_id')  # 获取URL中的请求参数
+    request_id = request.args.get('request_id')
     if request_id:
         requests_data = db.collection('requests').document(
             request_id).get().to_dict()
         qna_ref = db.collection('qna').where("request_id", "==", request_id)
         qna_data = []
-        for doc in qna_ref.stream():  # 遍历requests collection中的每个文档
+        for doc in qna_ref.stream():
             qna_data.append({"id": doc.id, **doc.to_dict()})
         return jsonify({"requests": requests_data, "qna": qna_data})
     else:
         requests_ref = db.collection('requests').order_by(
             'created_at', direction=firestore.Query.DESCENDING).limit(10)
         requests_data = []
-        for doc in requests_ref.stream():  # 遍历requests collection中的每个文档
+        for doc in requests_ref.stream():
             requests_data.append({"id": doc.id, "doc": doc.to_dict()})
         return jsonify(requests_data)
-
-    # global global_questions_store
-    # global global_answers_store
-    # json_data = {
-    #     "emails": global_emails_store,
-    #     "sheets": global_sheets_store,
-    #     "questions": global_questions_store,
-    #     "answers": global_answers_store
-    # }
-    # return jsonify(json_data)
 
 
 @main_routes.route('/parse_excel', methods=['POST'])
@@ -75,41 +58,43 @@ def parse_excel_route():
 
 @main_routes.route('/ask_all_questions', methods=['POST'])
 def ask_all_questions_route():
+    # Get the Authorization header value
+    auth_header = request.headers.get("Authorization")
+    # Check if the header value exists
+    if not auth_header:
+        return jsonify({"error": "Authorization header is required"}), 401
+    # Extract the token by splitting the header value by whitespace (assuming "Bearer" scheme)
+    auth_token = auth_header.split(" ")[1]
+    is_auth_token_valid = auth_token == os.environ.get("ACCESS_CODE")
+    if not is_auth_token_valid:
+        return jsonify({"error": "Authorization is not valid"}), 403
+
     data = request.get_json()
+    config = data.get("config", {})
     email = data.get('email', [])
     sheets = data.get('sheets', [])
     questions = data.get('questions', [])
-    # request_id = uuid.uuid4().hex
 
     request_data = {
         "email": email,
+        "config": config,
         "sheets": sheets,
-        "questions": questions,
-        "answers": [],
-        "created_at": firestore.SERVER_TIMESTAMP,
         "questions_count": len(questions),
         "success_count": 0,
         "fail_count": 0,
+        "created_at": firestore.SERVER_TIMESTAMP,
     }
-    # db.collection('requests').document(request_id).set(request_data)
-    created_at, request_ref = db.collection('requests').add(request_data)
+    _, request_ref = db.collection('requests').add(request_data)
+
     request_id = request_ref.id
-
-    # global global_emails_store
-    # global_emails_store[request_id] = email
-
-    # global global_sheets_store
-    # global_sheets_store[request_id] = sheets
 
     tasks_client = tasks_v2.CloudTasksClient()
     parent = tasks_client.queue_path(
         'withcontextai', 'us-west1', 'chat-completions-queue')
 
-    for i, question in enumerate(questions):
+    for _, question in enumerate(questions):
         question_id = question.get("id")
         question_text = question.get("text")
-        # question_id = uuid.uuid4().hex
-        # question_text = question
 
         payload = json.dumps({
             "request_id": request_id,
@@ -117,19 +102,13 @@ def ask_all_questions_route():
             "question_text": question_text
         })
 
-        # global global_questions_store
-        # if request_id not in global_questions_store:
-        #     global_questions_store[request_id] = []
-        # global_questions_store[request_id].append(
-        #     {"id": question_id, "text": question_text})
-
         task = {
             'http_request': {
                 'http_method': 'POST',
-                'url': 'https://openai-tools-mmxbwgwwaq-uw.a.run.app/chat_completions_async',
+                'url': api_url + '/chat_completions_async',
                 'headers': {
                     'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + os.environ.get("ACCESS_CODE"),
+                    'Authorization': 'Bearer ' + auth_token,
                 },
                 'body': payload.encode(),
             }
@@ -142,14 +121,6 @@ def ask_all_questions_route():
 
 @main_routes.route('/chat_completions_async', methods=['POST'])
 def chat_completions_async_route():
-    data = request.get_json()
-    request_id = data.get("request_id")
-    question_id = data.get("question_id")
-    question_text = data.get("question_text")
-
-    if not request_id or not question_id or not question_text:
-        return jsonify({"error": "Data missing: request_id, question_id, or question_text"}), 400
-
     # Get the Authorization header value
     auth_header = request.headers.get("Authorization")
 
@@ -167,15 +138,25 @@ def chat_completions_async_route():
     if not request.is_json:
         return jsonify({"error": "JSON data expected"}), 400
 
-    # data = request.get_json()
+    data = request.get_json()
+    request_id = data.get("request_id")
+    question_id = data.get("question_id")
+    question_text = data.get("question_text")
 
-    model = data.get("model", "gpt-3.5-turbo")
-    # messages = data.get("messages", [])
-    messages = [{"role": "system", "content": "You are ChatGPT, a large language model trained by OpenAI."}, {
-        "role": "user", "content": question_text}]
-    temperature = data.get("temperature", 0.7)
-    presence_penalty = data.get("presence_penalty", 0)
-    frequency_penalty = data.get("frequency_penalty", 0)
+    if not request_id or not question_id or not question_text:
+        return jsonify({"error": "Data missing: request_id, question_id, or question_text"}), 400
+
+    request_data = db.collection('requests').document(
+        request_id).get().to_dict()
+    config = request_data.get("config", {})
+    model = config.get("model", "gpt-3.5-turbo")
+    system_message = config.get(
+        "system", "You are ChatGPT, a large language model trained by OpenAI.")
+    messages = [{"role": "system", "content": system_message},
+                {"role": "user", "content": question_text}]
+    temperature = config.get("temperature", 0.7)
+    presence_penalty = config.get("presence_penalty", 0)
+    frequency_penalty = config.get("frequency_penalty", 0)
 
     if not (model and messages):
         return jsonify({"error": "model and messages must be provided"}), 400
@@ -190,7 +171,6 @@ def chat_completions_async_route():
         )
         db.collection('requests').document(request_id).update(
             {"success_count": firestore.Increment(1)})
-        # return jsonify(response)
     except Exception as e:
         db.collection('requests').document(request_id).update(
             {"fail_count": firestore.Increment(1)})
@@ -199,7 +179,7 @@ def chat_completions_async_route():
     answer_text = response.get("choices", [])[0].get(
         "message", {}).get("content")
 
-    _, qna_ref = db.collection('qna').add({
+    db.collection('qna').add({
         "request_id": request_id,
         "question_id": question_id,
         "question_text": question_text,
@@ -210,13 +190,10 @@ def chat_completions_async_route():
 
     request_data = db.collection('requests').document(
         request_id).get().to_dict()
-
-    # db.collection('qna').document(question_id).update(
-    #     {"answer_text": answer_text})
-
     questions_count = request_data.get("questions_count", 0)
     success_count = request_data.get("success_count", 0)
     print(questions_count, success_count)
+
     if request_data is not None and success_count == questions_count:
         payload = json.dumps({
             "request_id": request_id,
@@ -225,7 +202,7 @@ def chat_completions_async_route():
         task = {
             'http_request': {
                 'http_method': 'POST',
-                'url': 'https://openai-tools-mmxbwgwwaq-uw.a.run.app/send_answers_email',
+                'url': api_url + '/send_answers_email',
                 'headers': {
                     'Content-Type': 'application/json'
                 },
@@ -238,103 +215,7 @@ def chat_completions_async_route():
             'withcontextai', 'us-west1', 'send-answers-email-queue')
         tasks_client.create_task(request={'parent': parent, 'task': task})
 
-    # Process the question and return a random answer for demonstration purposes
-    # answer_choice = random.choice(['Yes', 'No', 'Maybe'])
-    # if answer_choice == "Maybe":
-    # return jsonify({"error": "Cannot answer with Maybe"}), 400
-    # answer = f"Answer to question {question_id}: {answer_choice}"
-
-    # Format the answer and other metadata as a JSON string
-    # payload = json.dumps({
-    #     "request_id": request_id,
-    #     # "question_id": question_id,
-    #     "question_id": qna_ref.id,
-    #     "answer_text": answer
-    # })
-
-    # task = {
-    #     'http_request': {
-    #         'http_method': 'POST',
-    #         'url': 'https://openai-tools-mmxbwgwwaq-uw.a.run.app/answer_collector',
-    #         'headers': {
-    #             'Content-Type': 'application/json'
-    #         },
-    #         'body': payload.encode(),
-    #     }
-    # }
-
-    # tasks_client = tasks_v2.CloudTasksClient()
-    # parent = tasks_client.queue_path(
-    #     'withcontextai', 'us-west1', 'answer-collector-queue')
-    # tasks_client.create_task(request={'parent': parent, 'task': task})
-
     return jsonify({"message": f"Answer published for question {question_id}"}), 200
-
-
-# @main_routes.route('/answer_collector', methods=['POST'])
-# def answer_collector_route():
-#     data = request.get_json()
-
-#     # 从请求中获取 request_id, question_id 和 answer_text
-#     request_id = data.get("request_id")
-#     question_id = data.get("question_id")
-#     answer_text = data.get("answer_text")
-
-#     request_data = db.collection('requests').document(
-#         request_id).get().to_dict()
-
-#     db.collection('qna').document(question_id).update(
-#         {"answer_text": answer_text})
-
-#     # answers = request_data["answers"]
-#     # answers.append({"id": question_id, "text": answer_text})
-#     # db.collection('requests').document(request_id).update({"answers": answers})
-
-#     # questions = request_data["questions"]
-#     # questions = [
-#     #     question for question in questions if question["id"] != question_id]
-#     # db.collection('requests').document(
-#     #     request_id).update({"questions": questions})
-
-#     # # 添加 answer_text 到全局变量中
-#     # global global_answers_store
-#     # if request_id not in global_answers_store:
-#     #     global_answers_store[request_id] = []
-#     # global_answers_store[request_id].append(
-#     #     {"id": question_id, "text": answer_text})
-
-#     # # 删除 global_questions_store 中的对应 question_id 的数据
-#     # global global_questions_store
-#     # if request_id in global_questions_store:
-#     #     global_questions_store[request_id] = [
-#     #         question for question in global_questions_store[request_id] if question["id"] != question_id]
-
-#     # 如果问题都回答完了，触发发送邮件任务
-#     # if request_id in global_questions_store and len(global_questions_store[request_id]) == 0:
-#     questions_count = request_data.get("questions_count", 0)
-#     success_count = request_data.get("success_count", 0)
-#     if request_data is not None and success_count == questions_count:
-#         payload = json.dumps({
-#             "request_id": request_id,
-#         })
-
-#         task = {
-#             'http_request': {
-#                 'http_method': 'POST',
-#                 'url': 'https://openai-tools-mmxbwgwwaq-uw.a.run.app/send_answers_email',
-#                 'headers': {
-#                     'Content-Type': 'application/json'
-#                 },
-#                 'body': payload.encode(),
-#             }
-#         }
-
-#         tasks_client = tasks_v2.CloudTasksClient()
-#         parent = tasks_client.queue_path(
-#             'withcontextai', 'us-west1', 'send-answers-email-queue')
-#         tasks_client.create_task(request={'parent': parent, 'task': task})
-
-#     return jsonify({"message": f"Answer published for question {question_id}"}), 200
 
 
 @main_routes.route('/send_answers_email', methods=['POST'])
@@ -346,20 +227,14 @@ def send_answers_email_route():
 
     data = db.collection('requests').document(request_id).get().to_dict()
     sheets = data["sheets"]
-    # answers = data["answers"]
     email = data["email"]
 
     qna_ref = db.collection('qna').where("request_id", "==", request_id)
     answers = []
-    for doc in qna_ref.stream():  # 遍历requests collection中的每个文档
-        # question_id, answer_text = doc.to_dict()
+    for doc in qna_ref.stream():
         answer = doc.to_dict()
-        answers.append({"id": answer.get("question_id"), "text": answer.get("answer_text")})
-
-    # # 从全局变量中获取 sheets, answers 和 email
-    # sheets = global_sheets_store.get(request_id)
-    # answers = global_answers_store.get(request_id)
-    # email = global_emails_store.get(request_id)
+        answers.append({"id": answer.get("question_id"),
+                       "text": answer.get("answer_text")})
 
     # 整合 sheets 和 answers 数据
     for sheet in sheets:
